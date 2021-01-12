5cf5340e5e9b9252095c37b09bf362be122d512c
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2021-01-12 18:28:21 +0100

    Add mapping to project

---
+++ EWallet.Api/Maps/MappingProfiles.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using EWallet.Api.DTOs;
+using EWallet.Api.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.WebAPI.Maps
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<UserToRegisterDTO, User>();
+            CreateMap<WalletToAdd, Wallet>();
+            CreateMap<User, UserToReturn>();
+        }
+    }
+}
