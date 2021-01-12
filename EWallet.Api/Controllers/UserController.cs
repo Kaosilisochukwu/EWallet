0fa1e76bc7d15e8966a70a0c1cfe62807f446e39
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2021-01-12 18:27:21 +0100

    Add project controllers with some endpoint implementation

---
+++ EWallet.Api/Controllers/UserController.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using EWallet.Api.DTOs;
+using EWallet.Api.Model;
+using EWallet.Api.Utils;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EWallet.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IConfiguration _config;
+        private readonly IMapper _mapper;
+
+        public UserController( UserManager<User> userManager, SignInManager<User> signInManager, IHttpContextAccessor httpContextAccessor, IConfiguration config, IMapper mapper)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _httpContextAccessor = httpContextAccessor;
+            _config = config;
+            _mapper = mapper;
+        }
+        [HttpPost]
+        [Route("register", Name = "register")]
+        public async Task<IActionResult> RegisterUser([FromBody] UserToRegisterDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _mapper.Map<User>(model);
+                var registrationResult = await _userManager.CreateAsync(user, model.Password);
+                if (registrationResult.Succeeded)
+                {
+                    var userToReturn = _mapper.Map<UserToReturn>(user);
+                    return Created("register", new ResponseModel(201, "User Was successfully registered", userToReturn));
+                }
+                var error = registrationResult.Errors;
+                return BadRequest(new ResponseModel(400, "Registration Failed", error));
+            }
+            var errors = ModelState.Values.Select(model => model.Errors).ToList();
+            return BadRequest(new ResponseModel(400, "There are some validation Errors", errors));
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] UserToLoginDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                var signInResult = await _signInManager.PasswordSignInAsync(user, model.Password, true, false);
+                if (signInResult.Succeeded)
+                {
+                    var token = TokenConfig.GenerateToken(user, _config);
+                    return Ok(new ResponseModel(200, "User Was successfully Logged in", new { token }));
+                }
+                return BadRequest(new ResponseModel(400, "Login Failed", signInResult));
+            }
+            var errors = ModelState.Values.Select(model => model.Errors).ToList();
+            return BadRequest(new ResponseModel(400, "There are some validation Errors", errors));
+        }
+    }
+}
---
+++ EWallet.Api/Controllers/WalletController.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using EWallet.Api.DTOs;
+using EWallet.Api.Model;
+using EWallet.Api.Services.Repositories;
+using EWallet.Api.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace EWallet.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class WalletController : ControllerBase
+    {
+        private readonly WalletRepository _walletRepo;
+        private readonly IHttpContextAccessor _httpContext;
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+
+        public WalletController(WalletRepository walletRepo, IHttpContextAccessor httpContext, IMapper mapper, UserManager<User> userManager)
+        {
+            _walletRepo = walletRepo;
+            _httpContext = httpContext;
+            _mapper = mapper;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        [Route("add", Name = "add")]
+        public async Task<IActionResult> AddWallet(WalletToAdd model)
+        {
+            if (ModelState.IsValid)
+            {
+                var userId = _httpContext.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+                var wallet = _mapper.Map<Wallet>(model);
+                var user = await _userManager.FindByIdAsync(userId);
+                var failureResponseMessage = user.Role == "Noob" ? "Noob Users can only have one account" 
+                                        : user.Role == "Admin" ? "Admins are not allowed to have an account"
+                                        : "Wallet not added";
+                wallet.UserId = userId;
+                var addResult = await _walletRepo.AddWAllet(wallet);
+                if (addResult > 0)
+                {
+                    return Created("add", new ResponseModel(201, "Wallet Successfully added", wallet));
+                }
+                return BadRequest(new ResponseModel(400, failureResponseMessage, wallet));
+            }
+            var errors = ModelState.Values.Select(model => model.Errors).ToList();
+            return BadRequest(new ResponseModel(400, "There are some validation Errors", errors));
+        }
+
+        [HttpPost]
+        [Route("currency")]
+        public async Task<IActionResult> ChangeWalletMainCurrency(string newCurrency)
+        {
+            var userId = _httpContext.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var currencyChangeResult = await _walletRepo.ChangeMainCurrency(newCurrency, userId);
+            if(currencyChangeResult == 2)
+            {
+                return Ok(new ResponseModel(200, "Main Currency Successfully changed", newCurrency));
+            }
+            return BadRequest(new ResponseModel(400, "Failed to update", newCurrency));
+        }
+
+        [HttpPost]
+        [Route("fund")]
+        public async Task<IActionResult> FundWallet(FundRequestDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            var errors = ModelState.Values.Select(model => model.Errors).ToList();
+            return BadRequest(new ResponseModel(400, "There are some validation Errors", errors));
+        }
+    }
+}
